f7d49cfd9dc467cd2549f78c0d04f625d9a4093d
a.SOUFIANE <s.aithmad2425@uca.ac.ma>
2025-03-14 12:02:32 +0000

    Update Depense.java

--- src/beans/Depense.java
+++ src/beans/Depense.java
@@ -3,13 +3,11 @@ package beans;
 import java.util.Date;
 
 public class Depense {
-    private int id;
     private String libelle;
     private double montant;
     private Date date;
 
-    public Depense(int id, String libelle, double montant, Date date) {
-        this.id = id;
+    public Depense(String libelle, double montant, Date date) {
         this.libelle = libelle;
         this.montant = montant;
         this.date = date;
@@ -21,8 +19,6 @@ public class Depense {
         this.date = date;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
     public String getLibelle() { return libelle; }
     public void setLibelle(String libelle) { this.libelle = libelle; }
     public double getMontant() { return montant; }
