b77cc83bdc6a0fefff2185ef04831b3d36d6bf81
a.SOUFIANE <s.aithmad2425@uca.ac.ma>
2025-03-14 12:33:23 +0000

    Update Paiement.java

--- src/beans/Paiement.java
+++ src/beans/Paiement.java
@@ -1,17 +1,9 @@
 package beans;
 
 public class Paiement {
-    private int id;
     private Depense depense;
     private Etudiant etudiant;
     private String statut;
-
-    public Paiement(int id, Depense depense, Etudiant etudiant, String statut) {
-        this.id = id;
-        this.depense = depense;
-        this.etudiant = etudiant;
-        this.statut = statut;
-    }
 
     public Paiement(Depense depense, Etudiant etudiant, String statut) {
         this.depense = depense;
@@ -19,12 +11,13 @@ public class Paiement {
         this.statut = statut;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
     public Depense getDepense() { return depense; }
     public void setDepense(Depense depense) { this.depense = depense; }
+
     public Etudiant getEtudiant() { return etudiant; }
     public void setEtudiant(Etudiant etudiant) { this.etudiant = etudiant; }
+
     public String getStatut() { return statut; }
     public void setStatut(String statut) { this.statut = statut; }
 }
+
