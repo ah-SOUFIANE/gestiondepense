a1a7c8dca0708bdb9e16c80d2b2be3d5f97243ee
a.SOUFIANE <s.aithmad2425@uca.ac.ma>
2025-03-15 09:05:15 +0000

    Update Etudiant.java

--- src/beans/Etudiant.java
+++ src/beans/Etudiant.java
@@ -21,14 +21,12 @@ public class Etudiant {
 
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-
     public String getNom() { return nom; }
     public void setNom(String nom) { this.nom = nom; }
-
     public String getPrenom() { return prenom; }
     public void setPrenom(String prenom) { this.prenom = prenom; }
-
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 }
+
 
